b33a09b0cc291d0653c82abe44e02291c0329a3d
qwerty1121gif <Vladimirstalin@tutamail.com>
2024-11-29 20:17:59 +0300

    commit

--- lion.py
+++ lion.py
@@ -7,9 +7,12 @@ file_path = 'lion.docx'
 doc = docx.Document(file_path)
 text = ' '.join([paragraph.text for paragraph in doc.paragraphs]).lower()
 
-words = pd.Series(re.findall(r'\b\w+\b', text)).value_counts()
-df_words = words.rename_axis('Слово').reset_index(name='Частота')
-df_words['Процент'] = (df_words['Частота'] / len(words)) * 100
+words = pd.Series(re.findall(r'\b\w+\b', text))
+total_words = len(words)
+word_counts = words.value_counts()
+df_words = word_counts.rename_axis('Слово').reset_index(name='Частота')
+df_words['Процент'] = (df_words['Частота'] / total_words) * 100
+
 print(df_words)
 df_words.to_csv('word_frequency.csv', index=False, encoding='utf-8')
 print(f'Результаты частоты слов сохранены в файл: word_frequency.csv')
